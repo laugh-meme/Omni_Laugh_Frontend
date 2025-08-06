ed9c9fc6f2ef4e9fcbbf8125e579ca8ec6cbf784
fre2dom0 <fre2dom0@gmail.com>
2025-08-06 22:57:38 +0300

    Unused import removed

--- src/pages/Tokenomics.tsx
+++ src/pages/Tokenomics.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import AccordionComp from "../components/AccordionComponents/AccordionComponent";
 
